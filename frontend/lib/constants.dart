6c7007195cd45bd6e2a26d1340b7bd5752d4199b
Mamzilion <danielmama881@gmail.com>
2026-03-29 12:26:29 +0200

    Update constants.dart

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  // REMPLACE PAR TON IP (ex: 192.168.1.15)
+  // REMPLACE PAR TON IP_ip du serveur (ex: 192.168.1.15)
   static const String ipAddress = "10.207.5.169"; 
   static const String baseUrl = "http://$ipAddress:5000/api";
 }
